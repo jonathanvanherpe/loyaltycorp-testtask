1c120faa5ac94aeb956fe7c567beb1878d454d79
Jonathan Vanherpe <git@freebase.be>
2018-05-21 16:49:35 +1000

    add MailchimpMember entity

---
+++ app/Database/Entities/MailChimp/MailChimpMember.php
@@ -0,0 +1,144 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Database\Entities\MailChimp;
+
+use Doctrine\ORM\Mapping as ORM;
+use EoneoPay\Utils\Str;
+
+/**
+ * @ORM\Entity()
+ */
+class MailChimpMember extends MailChimpEntity
+{
+    /**
+     * @ORM\Column(name="email_address", type="string")
+     *
+     * @var string
+     */
+    private $emailAddress;
+
+    /**
+     * @ORM\Column(name="status", type="string")
+     *
+     * @var string
+     */
+    private $status;
+
+    /**
+     * @ORM\Id()
+     * @ORM\Column(name="id", type="guid")
+     * @ORM\GeneratedValue(strategy="UUID")
+     *
+     * @var string
+     */
+    private $memberId;
+
+
+    /**
+     * @ORM\Column(name="mail_chimp_id", type="string", nullable=true)
+     *
+     * @var string
+     */
+    private $mailChimpId;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="MailChimpList")
+     *
+     * @ORM\JoinColumn(name="mail_chimp_list_id", referencedColumnName="id")
+     */
+    private $mailChimpList;
+
+    /**
+     * Get id.
+     *
+     * @return null|string
+     */
+    public function getId(): ?string
+    {
+        return $this->memberId;
+    }
+
+    /**
+     * Get mailchimp id of the member.
+     *
+     * @return null|string
+     */
+    public function getMailChimpId(): ?string
+    {
+        return $this->mailChimpId;
+    }
+
+    /**
+     * Set mailchimp id of the member.
+     *
+     * @param string $mailChimpId
+     *
+     * @return \App\Database\Entities\MailChimp\MailChimpMember
+     */
+    public function setMailChimpId(string $mailChimpId): MailChimpMember
+    {
+        $this->mailChimpId = $mailChimpId;
+
+        return $this;
+    }
+
+    /**
+     * Get validation rules for mailchimp entity.
+     *
+     * @return array
+     */
+    public function getValidationRules(): array
+    {
+        return [
+            'emailaddress' => 'required|string',
+            'status' => 'required|string',
+            'mailchimp_id' => 'nullable|string',
+        ];
+    }
+
+    /**
+     * Set emailAddress.
+     *
+     * @param string $emailAddress
+     *
+     * @return MailChimpMember
+     */
+    public function setEmailAddress(string $emailAddress): MailChimpMember
+    {
+        $this->emailAddress = $emailAddress;
+
+        return $this;
+    }
+
+    /**
+     * Set status.
+     *
+     * @param string $status
+     *
+     * @return MailChimpMember
+     */
+    public function setStatus(string $status): MailChimpMember
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get array representation of entity.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        $array = [];
+        $str = new Str();
+
+        foreach (\get_object_vars($this) as $property => $value) {
+            $array[$str->snake($property)] = $value;
+        }
+
+        return $array;
+    }
+}
